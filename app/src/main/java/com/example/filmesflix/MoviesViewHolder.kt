cb2ee881df4cbc8eebaa05dacd4830f043eabf7b
Eltuss <elton.s.barbosa@outlook.com>
2021-08-20 15:24:47 -0300

    First commit

---
+++ app/src/main/java/com/example/filmesflix/MoviesViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.filmesflix
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
